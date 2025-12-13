dd203d052542d8d20197018aef59d6362101a909
nikiiigoyal <nikiiigoyal@gmail.com>
2025-12-13 17:20:58 +0530

    feat/ - Add new machine and button img

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import './App.css'
+import Reel from './Reel';
+
+function App() {
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const [results, setResults] = useState([1, 1, 1]); 
+
+  const handleSpin = () => {
+    if (isSpinning) return;
+    setIsSpinning(true);
+
+    // 1. Generate 3 Random Numbers Locally
+    const newResults = [
+      Math.floor(Math.random() * 9) + 1,
+      Math.floor(Math.random() * 9) + 1,
+      Math.floor(Math.random() * 9) + 1
+    ];
+    setResults(newResults);
+
+    // 2. Reset Button after longest animation (approx 5.5 sec)
+    setTimeout(() => {
+      setIsSpinning(false);
+    }, 5500);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 ">
+      
+      {/* MACHINE CONTAINER */}
+      <div className='relative w-[400px]'> 
+        
+        {/* Background Image */}
+        <img src="/Vending-machine.svg" alt="Vending Machine" className='w-full h-auto drop-shadow-2xl z-10 relative pointer-events-none'/> 
+        
+        {/* --- SCREEN WINDOW (Grid of 3 Reels) --- */}
+        
+        <div className='absolute top-[18%] left-[17%] w-[64%] h-[200px] flex flex-row overflow-hidden border-none outline-none z-20'>
+            
+            {/* Reel 1 (No delay) */}
+            <Reel targetNumber={results[0]} isSpinning={isSpinning} delay={0} />
+            
+            {/* Reel 2 (0.5s delay) */}
+            <Reel targetNumber={results[1]} isSpinning={isSpinning} delay={500} />
+            
+            {/* Reel 3 (1s delay) */}
+            <Reel targetNumber={results[2]} isSpinning={isSpinning} delay={1000} />
+
+        </div>
+
+        {/* --- BUTTON --- */}
+        <div className='absolute -bottom-[14%] right-[37%] z-30'>
+          <button 
+            onClick={handleSpin} 
+            disabled={isSpinning} 
+           className={`
+      transition-all duration-100 ease-in-out
+      active:translate-y-2 
+      ${isSpinning ? 'opacity-10 cursor-not-allowed' : 'cursor-pointer hover:brightness-110'}
+    `}
+            title='Click to spin'
+          >
+            <img src="/red-spin-button.png" 
+      alt="Spin Button" 
+      className="w-53 h-117 object-contain drop-shadow-xl"></img>
+          </button>
+        </div>
+
+      </div>  
+    </div>
+  )
+}
+
+export default App
---
+++ src/Reel.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+
+const Reel = ({ targetNumber, isSpinning, delay }) => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numberColors = {
+    1: 'text-[#E237FF]',      
+    2: 'text-[#FF9635]',     
+    3: 'text-[#E237FF]',    
+    4: 'text-[#FF9635]',   
+    5: 'text-[#FF73D9]',   
+    6: 'text-white',    
+    7: 'text-white',     
+    8: 'text-[#FF73D9]',
+    9: 'text-[#3BF0FF]',   
+  };
+  const spinNumbers = [...numbers, ...numbers, ...numbers, ...numbers, ...numbers, ...numbers, ...numbers];
+  
+  const Num_Height = 65;  
+  const [stripPosition, setStripPosition] = useState(0);
+  const [transitionDuration, setTransitionDuration] = useState('0s');
+
+  useEffect(() => {
+    if (isSpinning && targetNumber !== null) {
+      // --- STEP 1: 
+      setTransitionDuration('0s');
+      setStripPosition(0);
+
+      // --- STEP 2: SPIN 
+      setTimeout(() => {
+        const targetIndex = spinNumbers.length - 15 + (targetNumber - 1);
+        const rawPosition = -(targetIndex * Num_Height);
+        const centerPosition = rawPosition + Num_Height; 
+
+        // Ab animation time set karo
+        const duration = 4000 + delay; 
+        setTransitionDuration(`${duration}ms`);
+        
+        // Aur nayi position par jao
+        setStripPosition(centerPosition);
+      }, 50);
+
+    } 
+    // Note: 'else if' hata diya kyunki hum shuru mein hi reset kar rahe hain
+  }, [isSpinning, targetNumber, delay]);
+
+  return (
+    <div className="relative overflow-hidden h-[180px] w-full">
+      {/* Moving Strip */}
+      <div 
+        className="flex flex-col items-center w-full"
+        style={{ 
+          transform: `translateY(${stripPosition}px)`,
+          transitionProperty: 'transform',
+          transitionDuration: transitionDuration,
+          transitionTimingFunction: 'cubic-bezier(0.25, 1, 0.5, 1)' // Mechanical ease-out
+        }}
+      >
+        {spinNumbers.map((num, index) => (
+          <div 
+            key={index} 
+            className={`flex items-center   font-skranji justify-center font-bold ${numberColors[num]}`}
+            style={{ height: `${Num_Height}px`, fontSize: '40px' }}
+          >
+            {num}
+          </div>
+        ))}
+      </div>
+      
+      
+      <div className="absolute top-[80px] w-full h-[80px]  bg-yellow-100/10 pointer-events-none"></div>
+    </div>
+  );
+};
+
+export default Reel;
